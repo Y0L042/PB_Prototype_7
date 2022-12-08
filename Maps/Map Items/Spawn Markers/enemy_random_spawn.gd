de928210f6230c5d6a49e32fce6eb9e499b78ad7
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-12-08 15:22:53 +0200

    QS: Fixed enemy_random_spawn timing , & added disabled zone

--- Maps/Map Items/Spawn Markers/enemy_random_spawn.gd
+++ Maps/Map Items/Spawn Markers/enemy_random_spawn.gd
@@ -3,6 +3,8 @@ extends Marker2D
 
 var _parent
 var active_instances: Array = []
+@onready var timer: Timer = $Timer
+
 @export var _troops: Array[Resource] : get = get_troops
 @export var _ai: Resource
 #@export_category("Spawn Config")
@@ -10,9 +12,15 @@ var active_instances: Array = []
 @export var waves: int = -1
 @export var max_active_instances: int = -1
 @export var active: bool = true
+@export_group("Event Settings")
+@export var enable_disable_zone: bool = false
+@export var disable_zone: Area2D
+
 
 func set_delay(new_delay: float):
 	delay = new_delay
+	if timer != null:
+		timer.set_wait_time(delay)
 
 func set_waves(new_waves: int):
 	waves = new_waves
@@ -23,29 +31,52 @@ func set_max_active_instances(new_max_active_instances: int):
 
 func _ready() -> void:
 	_parent = get_parent()
-	_custom_process()
+	timer.timeout.connect(timer_timeout)
+	timer.start(delay)
+	if enable_disable_zone and disable_zone != null:
+		disable_zone.area_entered.connect(disable_zone_entered)
+		disable_zone.area_exited.connect(disable_zone_exited)
 
-func _custom_process():
-
-	while active:
-		var temp_timer = get_tree().create_timer(delay)
-		await temp_timer.timeout
+func timer_timeout():
+	if active:
 		if waves == 0:
 			active = false
 			return
 
-		var skip: bool = false
+		var LIMIT_REACHED: bool = false
 		if !active_instances.is_empty() and max_active_instances == active_instances.size():
-			skip = true
-		else: skip = false
-
-		if !skip:# dirty hack. try to use process or something to solve return problem
+			LIMIT_REACHED = true
+		else: LIMIT_REACHED = false
+		if !LIMIT_REACHED:# dirty hack. try to use process or something to solve return problem
 			var army = _spawn_enemies()
 			army._soldier_manager.ArmyIsDefeated.connect(remove_army_from_active_instances)
 			active_instances.append(army)
-
 			if waves != -1:
 				waves -= 1
+	timer.start(delay)
+
+func _physics_process(delta: float) -> void:
+#	random_spawning()
+	pass
+
+func random_spawning():
+	var temp_timer = get_tree().create_timer(delay)
+	await temp_timer.timeout
+	if waves == 0:
+		active = false
+		return
+
+	if !active_instances.is_empty() and max_active_instances == active_instances.size():
+		active = true
+	else: active = false
+
+	if !active:# dirty hack. try to use process or something to solve return problem
+		var army = _spawn_enemies()
+		army._soldier_manager.ArmyIsDefeated.connect(remove_army_from_active_instances)
+		active_instances.append(army)
+
+		if waves != -1:
+			waves -= 1
 
 
 
@@ -70,7 +101,19 @@ func get_troops():
 	return _troops
 
 
+func disable_zone_entered(area: Area2D):
+	var spotted_object = area.get_parent()
+	if spotted_object.faction == "Player":
+		active = false
+		print("zone entered")
 
+
+
+func disable_zone_exited(area: Area2D):
+	var spotted_object = area.get_parent()
+	if spotted_object.faction == "Player":
+		active = true
+		print("zone exited")
 
 
 
