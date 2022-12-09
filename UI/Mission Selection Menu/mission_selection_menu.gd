deae8a9c48f9a3318132a3b960a0b461ab11c1e7
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-12-09 16:24:14 +0200

    Update mission_selection_menu.gd

--- UI/Mission Selection Menu/mission_selection_menu.gd
+++ UI/Mission Selection Menu/mission_selection_menu.gd
@@ -6,6 +6,5 @@ func _ready() -> void:
 	btn_back.pressed.connect(_on_back_button_pressed)
 
 func _on_back_button_pressed() -> void:
-	SceneLib.spawn_child(SceneLib.MAIN_MENU, get_parent())
-	self.queue_free()
+	SceneManager.
 
